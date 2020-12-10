748ef2b1c90cce549af3de7fbc880534b512ecf0
Jelle Bruisten <jelle_bruisten@hotmail.com>
2020-12-10 02:50:57 +0100

    - created book feature

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -5,14 +5,18 @@ import { BookRoutingModule } from './book-routing.module';
 import { OverviewComponent } from './components/overview/overview.component';
 import { CreateComponent } from './components/create/create.component';
 import { ViewComponent } from './components/view/view.component';
-
-
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './reducers/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './effects/book.effects';
 
 @NgModule({
   declarations: [OverviewComponent, CreateComponent, ViewComponent],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    StoreModule.forFeature(fromBook.bookFeatureKey, fromBook.reducer),
+    EffectsModule.forFeature([BookEffects])
   ]
 })
 export class BookModule { }
